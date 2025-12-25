6d31496818aef1e171d9d256fbacd64a702da405
Dmytro <dimusgamer2.0@gmail.com>
2025-12-26 00:59:18 +0200

    fix: Make requested change

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,11 @@ const rows = document.querySelectorAll('table tr');
 
 rows.forEach((row) => {
   const cells = [...row.cells];
+
+  if (!cells[1]) {
+    return;
+  }
+
   const clonedCell = cells[1].cloneNode(true);
   const lastCell = cells.at(-1);
 
